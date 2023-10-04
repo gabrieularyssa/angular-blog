960832e4ded40b6f2b304f771aa1bef5e6d2dbe7
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-10-04 00:33:23 -0300

    components createds with styles

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SmallCardComponent,
+    BigCardComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
